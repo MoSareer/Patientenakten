c4acf809f9c60b8ba21899109ade7193169f6993
Mo,Sareer <119538510+MoSareer@users.noreply.github.com>
2022-12-06 22:55:42 +0100

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.IO;
+using System.Windows;
+using System.Text;
+
+namespace Patientenakten
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void erstellen_Click(object sender, RoutedEventArgs e)
+        {
+            string content = inhalt.Text;
+            string textdateie = dateiname.Text;
+            string dirPath = @"C:\Users\CC-Student\Desktop\";
+            
+
+            using (FileStream fs = File.Create(dirPath + textdateie + ".txt"))
+            {
+                byte[] contentToByte = Encoding.ASCII.GetBytes(content);
+                fs.Write(contentToByte, 0, contentToByte.Length);
+            }
+            
+            MessageBox.Show("Datei wurde angelegt.");
+        }
+
+        private void offnen_Click(object sender, RoutedEventArgs e)
+        {
+            string textdateie = dateiname.Text;
+            string dirPath = @"C:\Users\CC-Student\Desktop\";
+            using (FileStream fs = File.OpenRead(dirPath + textdateie + ".txt"))
+            {
+                using (StreamReader sr = new StreamReader(fs))
+                {
+                    string content = sr.ReadToEnd();
+                    inhalt.Text = content;
+                }
+            }
+            MessageBox.Show("Datei wurde gelesen.");
+
+        }
+    }
+}
